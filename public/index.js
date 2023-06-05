25afc7868897996384fb51821378f5b91d05d0cd
logan <loganh42@gmail.com>
2023-06-04 23:44:19 -0500

    Comment Out Visualizer

--- public/index.js
+++ public/index.js
@@ -23,11 +23,11 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
     // connect the analyser node to the destination of the audioContext constructor
     analyserNode.connect(audioContext.destination);
 
-    // canvas element in the static HTML file is called so the drawing can occur, has id='visualizationCanvas'
-    const waveformCanvas = document.getElementById('visualizationCanvas');
+    // // canvas element in the static HTML file is called so the drawing can occur, has id='visualizationCanvas'
+    // const waveformCanvas = document.getElementById('visualizationCanvas');
 
-    // using 2D visualization from the Canvas API
-    const waveformContext = waveformCanvas.getContext('2d');
+    // // using 2D visualization from the Canvas API
+    // const waveformContext = waveformCanvas.getContext('2d');
 
     // canvas element for the frequency bar graph
     const frequencyCanvas = document.getElementById('frequencyCanvas');
@@ -45,51 +45,51 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
       // pre-made method fills the audioArray with the analysed data
       analyserNode.getByteTimeDomainData(audioArray);
 
-      // clearing the waveform context
-      waveformContext.clearRect(0, 0, waveformCanvas.width, waveformCanvas.height);
+      // // clearing the waveform context
+      // waveformContext.clearRect(0, 0, waveformCanvas.width, waveformCanvas.height);
 
-      // background is grey colour
-      waveformContext.fillStyle = "rgb(200, 200, 200)";
+      // // background is grey colour
+      // waveformContext.fillStyle = "rgb(200, 200, 200)";
 
-      // fills the rectangle with the grey colour
-      waveformContext.fillRect(0, 0, waveformCanvas.width, waveformCanvas.height);
+      // // fills the rectangle with the grey colour
+      // waveformContext.fillRect(0, 0, waveformCanvas.width, waveformCanvas.height);
 
-      // line style for the waveform
-      waveformContext.lineWidth = 2; // 2 pixels
-      waveformContext.strokeStyle = 'rgb(0, 0, 0)';
+      // // line style for the waveform
+      // waveformContext.lineWidth = 2; // 2 pixels
+      // waveformContext.strokeStyle = 'rgb(0, 0, 0)';
 
-      // starting a new path
-      waveformContext.beginPath();
+      // // starting a new path
+      // waveformContext.beginPath();
 
-      /*
-        Determine the width of each segment of the line to be drawn by dividing the canvas width by the
-        array length (equal to the FrequencyBinCount, as defined earlier on), then define an x variable to
-        define the position to move to for drawing each segment of the line.
-      */
-      const sliceWidth = waveformCanvas.width / bufferLength;
-      let x = 0;
+      // /*
+      //   Determine the width of each segment of the line to be drawn by dividing the canvas width by the
+      //   array length (equal to the FrequencyBinCount, as defined earlier on), then define an x variable to
+      //   define the position to move to for drawing each segment of the line.
+      // */
+      // const sliceWidth = waveformCanvas.width / bufferLength;
+      // let x = 0;
 
-      /*
-        Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer
-        at a certain height based on the data point value from the array, then moving the line across to the place
-        where the next wave segment should be drawn
-      */
-      for (let i = 0; i < bufferLength; i++) {
-        const v = audioArray[i] / 128.0;
-        const y = (v * waveformCanvas.height) / 2;
+      // /*
+      //   Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer
+      //   at a certain height based on the data point value from the array, then moving the line across to the place
+      //   where the next wave segment should be drawn
+      // */
+      // for (let i = 0; i < bufferLength; i++) {
+      //   const v = audioArray[i] / 128.0;
+      //   const y = (v * waveformCanvas.height) / 2;
 
-        if (i === 0) {
-          waveformContext.moveTo(x, y);
-        } else {
-          waveformContext.lineTo(x, y);
-        }
+      //   if (i === 0) {
+      //     waveformContext.moveTo(x, y);
+      //   } else {
+      //     waveformContext.lineTo(x, y);
+      //   }
 
-        x += sliceWidth;
-      }
+      //   x += sliceWidth;
+      // }
 
-      // finish the line in the middle of the right-hand side of the canvas, then draw the stroke we've defined
-      waveformContext.lineTo(waveformCanvas.width, waveformCanvas.height / 2);
-      waveformContext.stroke();
+      // // finish the line in the middle of the right-hand side of the canvas, then draw the stroke we've defined
+      // waveformContext.lineTo(waveformCanvas.width, waveformCanvas.height / 2);
+      // waveformContext.stroke();
 
       // clear the frequency canvas
       frequencyContext.clearRect(0, 0, frequencyCanvas.width, frequencyCanvas.height);
