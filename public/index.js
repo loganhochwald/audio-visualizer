72f7b8cdd8ef353bcd316759cd55c78f68f7fcde
logan <loganh42@gmail.com>
2023-06-04 19:45:07 -0500

    Add Frequency Visualiser

--- public/index.js
+++ public/index.js
@@ -1,5 +1,3 @@
-// this is messy but I don't have a lot of time to fix this so it's all stuck in the .then() promise callback
-
 // requesting audio then getting to business, getUserMedia() method defined on object from Web Audio API
 navigator.mediaDevices.getUserMedia({ audio: true, video: false })
   .then((stream) => {
@@ -26,14 +24,17 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
     analyserNode.connect(audioContext.destination);
 
     // canvas element in the static HTML file is called so the drawing can occur, has id='visualizationCanvas'
-    const canvas = document.getElementById('visualizationCanvas');
+    const waveformCanvas = document.getElementById('visualizationCanvas');
 
     // using 2D visualization from the Canvas API
-    const canvasContext = canvas.getContext('2d');
+    const waveformContext = waveformCanvas.getContext('2d');
+
+    // canvas element for the frequency bar graph
+    const frequencyCanvas = document.getElementById('frequencyCanvas');
+    const frequencyContext = frequencyCanvas.getContext('2d');
 
     // Drawing loop to constantly read data
     const draw = () => {
-
       // frequency of bins returned by the FFT
       const bufferLength = analyserNode.frequencyBinCount;
 
@@ -44,56 +45,87 @@ navigator.mediaDevices.getUserMedia({ audio: true, video: false })
       // pre-made method fills the audioArray with the analysed data
       analyserNode.getByteTimeDomainData(audioArray);
 
-      // clearing the wave context
-      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+      // clearing the waveform context
+      waveformContext.clearRect(0, 0, waveformCanvas.width, waveformCanvas.height);
 
       // background is grey colour
-      canvasContext.fillStyle = "rgb(200, 200, 200)";
+      waveformContext.fillStyle = "rgb(200, 200, 200)";
 
       // fills the rectangle with the grey colour
-      canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-
+      waveformContext.fillRect(0, 0, waveformCanvas.width, waveformCanvas.height);
 
       // line style for the waveform
-      canvasContext.lineWidth = 2; // 2 pixels
-      canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+      waveformContext.lineWidth = 2; // 2 pixels
+      waveformContext.strokeStyle = 'rgb(0, 0, 0)';
 
       // starting a new path
-      canvasContext.beginPath();
+      waveformContext.beginPath();
 
-/*
-      Determine the width of each segment of the line to be drawn by dividing the canvas width by the
-      array length (equal to the FrequencyBinCount, as defined earlier on), then define an x variable to
-      define the position to move to for drawing each segment of the line.
-*/
-      const sliceWidth = canvas.width / bufferLength;
+      /*
+        Determine the width of each segment of the line to be drawn by dividing the canvas width by the
+        array length (equal to the FrequencyBinCount, as defined earlier on), then define an x variable to
+        define the position to move to for drawing each segment of the line.
+      */
+      const sliceWidth = waveformCanvas.width / bufferLength;
       let x = 0;
 
-/*
-      Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer
-      at a certain height based on the data point value from the array, then moving the line across to the place
-      where the next wave segment should be drawn
-*/
+      /*
+        Now we run through a loop, defining the position of a small segment of the wave for each point in the buffer
+        at a certain height based on the data point value from the array, then moving the line across to the place
+        where the next wave segment should be drawn
+      */
       for (let i = 0; i < bufferLength; i++) {
         const v = audioArray[i] / 128.0;
-        const y = (v * canvas.height) / 2;
+        const y = (v * waveformCanvas.height) / 2;
 
         if (i === 0) {
-          canvasContext.moveTo(x, y);
+          waveformContext.moveTo(x, y);
         } else {
-          canvasContext.lineTo(x, y);
+          waveformContext.lineTo(x, y);
         }
 
         x += sliceWidth;
       }
 
       // finish the line in the middle of the right-hand side of the canvas, then draw the stroke we've defined
-      canvasContext.lineTo(canvas.width, canvas.height / 2);
-      canvasContext.stroke();
+      waveformContext.lineTo(waveformCanvas.width, waveformCanvas.height / 2);
+      waveformContext.stroke();
+
+      // clear the frequency canvas
+      frequencyContext.clearRect(0, 0, frequencyCanvas.width, frequencyCanvas.height);
+
+      // background color for the frequency graph
+      frequencyContext.fillStyle = "rgb(200, 200, 200)";
+      frequencyContext.fillRect(0, 0, frequencyCanvas.width, frequencyCanvas.height);
+
+      // number of frequency bars to be displayed
+      const numBars = 100;
+
+      // width and height of each bar
+      const barWidth = frequencyCanvas.width / numBars;
+      const barHeightMultiplier = frequencyCanvas.height / 256;
+
+      // array to store frequency data
+      const frequencyArray = new Uint8Array(analyserNode.frequencyBinCount);
+
+      // get the frequency data
+      analyserNode.getByteFrequencyData(frequencyArray);
+
+      // loop to draw the frequency bars
+      for (let i = 0; i < numBars; i++) {
+        const frequencyValue = frequencyArray[Math.floor(i * (frequencyArray.length / numBars))];
+        const barHeight = frequencyValue * barHeightMultiplier;
+
+        // set color for the bar based on its height
+        frequencyContext.fillStyle = `rgb(${frequencyValue}, 0, 0)`;
+
+        // draw the bar
+        frequencyContext.fillRect(i * barWidth, frequencyCanvas.height - barHeight, barWidth, barHeight);
+      }
 
       // triggers the draw function again
       requestAnimationFrame(draw);
-    }
+    };
 
     // Start the drawing loop
     draw();
